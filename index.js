8d04a94447fb0d61ac01378dc84368c647fde558
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-09-25 10:46:00 +0200

    Restructured and commented

--- index.js
+++ index.js
@@ -3,22 +3,12 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const mongoString = process.env.DATABASE_URL;
+const app = express(); // Setting up an Express app
 
-mongoose
-  .connect(mongoString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(3000, () => {
-      console.log(`Server started at ${3000}`);
-    });
-  });
-const database = mongoose.connection;
-
-const app = express();
-
+/**
+ * Setting up the Express app with the necessary
+ * headers
+ */
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -33,10 +23,30 @@ app.use((req, res, next) => {
   next();
 });
 
+const mongoString = process.env.DATABASE_URL; // MongoDB Connection String
+
+/**
+ * Setting up MongoDB Database connection
+ */
+mongoose
+  .connect(mongoString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log(`Server started at ${3000}`);
+    });
+  });
+const database = mongoose.connection;
+
 database.on("error", (error) => {
   console.log(error);
 });
 
+/**
+ * Will print 'connected' if connection is successful
+ */
 database.once("connected", () => {
   console.log("Database Connected");
 });
