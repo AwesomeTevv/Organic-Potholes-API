3f0ce19725c609f1c8371594bef0334fda56b2e6
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-09-25 11:07:24 +0200

    Added the POST methods

---
+++ routes/potholeRoute.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const Pothole = require("../models/potholes");
+
+const router = express.Router();
+
+// Post Method
+// Creates a single Pothole and adds it to the database
+router.post("/addPothole", async (req, res) => {
+  const data = new Answer({
+    Coordinates: {
+      Longitude: req.body.Longitude,
+      Latitude: req.body.Latitude,
+    },
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Adds many Potholes and adds it to the database
+router.post("/addManyPotholes", async (req, res) => {
+  try {
+    const data = await Pothole.insertMany(req.body);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
